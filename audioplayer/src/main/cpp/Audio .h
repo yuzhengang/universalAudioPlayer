c050f8866f324a9ad4917d8418c493501eb60ff1
yuzhengang <1305164455@qq.com>
2019-10-28 14:57:37 +0800

    添加代码说明

---
+++ audioplayer/src/main/cpp/Audio .h
@@ -0,0 +1,46 @@
+//
+// Created by admin on 2019/10/28.
+//
+
+#ifndef UNIVERSALAUDIOPLAYER_AUDIO_H
+#define UNIVERSALAUDIOPLAYER_AUDIO_H
+
+#include "PlayerQueue.h"
+#include "Playstatus.h"
+#include "CallJava.h"
+#include "soundtouch/include/SoundTouch.h"
+#include "PlayBufferQueue.h"
+#include "PlayPcmBean.h"
+using  namespace  std;
+
+extern "C"
+{
+#include "include/libavcodec/avcodec.h"
+#include <include/libswresample/swresample.h>
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+#include <include/libavutil/time.h>
+};
+class  Audio{
+public:
+
+
+
+};
+#endif //UNIVERSALAUDIOPLAYER_AUDIO_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
